8c45d9cb2fd7771a05bfb4913c6ab2f6a539006b
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-10 10:57:19 -0700

    code challenge 1 is passing

---
+++ src/codeChallenge.js
@@ -0,0 +1,23 @@
+export function doubleNumbers(arr) {
+    return arr.map((num) => num * 2);
+}
+
+export function stringItUp(arr) {
+    return
+}
+
+export function capitalizeNames(arr) {
+    return
+}
+
+export function namesOnly(arr) {
+    return
+}
+
+export function makeStrings(arr){
+    return
+}
+
+export function readyToPutInTheDOM(arr) {
+    return
+}
---
+++ src/codeChallenge1.test.js
@@ -0,0 +1,10 @@
+import { doubleNumbers } from './codeChallenge.js';
+
+
+test('doubles numbers', () => {
+    const input = [2, 5, 100]; // arrange
+    const output = doubleNumbers(input); // act
+    expect(output).toEqual([4, 10, 200]); // assert
+});
+
+// test('')

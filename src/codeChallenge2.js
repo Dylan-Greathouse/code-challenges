4230f3febaef37dfbfe8fe88d3349a2b544e7b99
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-12 22:07:23 -0700

    got all filter tests passing

--- src/codeChallenge2.js
+++ src/codeChallenge2.js
@@ -1,3 +1,4 @@
+// filter method
 export function fiveAndGreaterOnly(arr) {
     return arr.filter((num) => num > 5)
 }
@@ -6,3 +7,14 @@ export function evensOnly(arr){
     return arr.filter((num) => num % 2 === 0)
 }
 
+export function fiveCharactersOrFewerOnly(arr){
+ return arr.filter((item) => item.length <= 5) 
+}
+
+export function peopleWhoBelongToTheIlluminati(arr){
+  return arr.filter((item) => item.member === true);
+}
+
+export function ofAge(arr){
+  return arr.filter((item) => item.age > 18);
+}
--- src/codeChallenge2.test.js
+++ src/codeChallenge2.test.js
@@ -1,4 +1,4 @@
-import { evensOnly, fiveAndGreaterOnly } from './codeChallenge2.js';
+import { evensOnly, fiveAndGreaterOnly, fiveCharactersOrFewerOnly, ofAge, peopleWhoBelongToTheIlluminati } from './codeChallenge2.js';
 
 test('Five and greater only', () => {
     const input = [3, 6, 8, 2]; // arrange
@@ -12,4 +12,39 @@ test('Five and greater only', () => {
       expect(output).toEqual([6, 8, 2]);
   })
 
-  
+  test('return wordss with five characters or less', () => {
+      const input = ['dog', 'wolf', 'by', 'family', 'eaten', 'camping'];
+      const output = fiveCharactersOrFewerOnly(input);
+      expect(output).toEqual(['dog', 'wolf', 'by', 'eaten']);
+  })
+
+  test('return people in illuminati', () => {
+      const input = [
+        { name: 'Angelina Jolie', member: true },
+        { name: 'Eric Jones', member: false },
+        { name: 'Paris Hilton', member: true },
+        { name: 'Kayne West', member: false },
+        { name: 'Bob Ziroll', member: true }
+      ];
+      const output = peopleWhoBelongToTheIlluminati(input);
+      expect(output).toEqual([ 
+        { name: 'Angelina Jolie', member: true }, 
+        { name: 'Paris Hilton', member: true }, 
+        { name: 'Bob Ziroll', member: true } 
+    ]);
+  })
+
+  test('return people who are older than 18', () => {
+      const input = [
+        { name: 'Angelina Jolie', age: 80 },
+        { name: 'Eric Jones', age: 2 },
+        { name: 'Paris Hilton', age: 5 },
+        { name: 'Kayne West', age: 16 },
+        { name: 'Bob Ziroll', age: 100 }
+    ];
+    const output = ofAge(input);
+    expect(output).toEqual([ 
+        { name: 'Angelina Jolie', age: 80 },
+        { name: 'Bob Ziroll', age: 100 } 
+    ])
+  })
